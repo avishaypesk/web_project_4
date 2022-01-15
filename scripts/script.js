e04be52fe9d897298ed69ffab2a24c191174a937
Avishay <avishaypesk@gmail.com>
2022-01-15 09:22:05 +0200

    added new cards form and intial cards to js

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,82 @@
-let editProfileButtonElement = document.querySelector(".profile__edit-button");
-let profileFormCloseButtonElement = document.querySelector(
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "./images/yosemite.jpg",
+  },
+  {
+    name: "Lake louise",
+    link: "./images/louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "./images/mountain.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "./images/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "./images/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "./images/lago.png",
+  },
+];
+
+const editProfileButtonElement = document.querySelector(
+  ".profile__edit-button"
+);
+const newCardButtonElement = document.querySelector(".profile__add-button");
+const profileFormCloseButtonElement = document.querySelector(
   ".form__close-button"
 );
-let userInputName = document.querySelector(".form__input_type_name");
-let userInputTitle = document.querySelector(".form__input_type_title");
-let profileName = document.querySelector(".profile__name");
-let profileTitle = document.querySelector(".profile__title");
-let popup = document.querySelector(".form");
-let profileForm = document.querySelector(".form__edit-profile");
+const userInputName = document.querySelector(".form__input_type_name");
+const userInputTitle = document.querySelector(".form__input_type_title");
+const profileName = document.querySelector(".profile__name");
+const profileTitle = document.querySelector(".profile__title");
+const profilePopup = document.querySelector(".form_type_profile");
+const newCardPopup = document.querySelector(".form_type_new-card");
+const profileForm = document.querySelector(".form__edit-profile");
+const newCardForm = document.querySelector(".form__new-card");
+const popups = document.querySelectorAll(".form");
 
-function openForm(form) {
-  form.classList.add("form_visible");
+function openPopup(popup) {
+  popup.classList.add("form_visible");
 }
 
-function closeForm(form) {
-  form.classList.remove("form_visible");
+function closePopup(popup) {
+  popup.classList.remove("form_visible");
 }
 
 function handleEditButtonClick() {
   userInputName.value = profileName.textContent;
   userInputTitle.value = profileTitle.textContent;
-  openForm(popup);
+  openPopup(profilePopup);
 }
 
 function handleEditFormSubmit(event) {
   event.preventDefault();
   profileName.textContent = userInputName.value;
   profileTitle.textContent = userInputTitle.value;
-  closeForm(popup);
+  closePopup(profilePopup);
+}
+
+function handleNewCardButtonClick() {
+  openPopup(newCardPopup);
 }
 
 editProfileButtonElement.addEventListener("click", handleEditButtonClick);
 profileForm.addEventListener("submit", handleEditFormSubmit);
-profileFormCloseButtonElement.addEventListener("click", () => closeForm(popup));
+newCardButtonElement.addEventListener("click", handleNewCardButtonClick);
+popups.forEach((popup) => {
+  popup.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("form_visible")) {
+      closePopup(popup);
+    }
+    if (evt.target.classList.contains("form__close-button")) {
+      closePopup(popup);
+    }
+  });
+});
