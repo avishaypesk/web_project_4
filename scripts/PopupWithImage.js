185a68a4f456d420dd1b599ae85bc4792ae02747
Avishay <avishaypesk@gmail.com>
2022-03-06 17:49:05 +0200

    refactor: add PopupWithImage class

---
+++ scripts/PopupWithImage.js
@@ -0,0 +1,29 @@
+import Popup from "./Popup";
+
+export default class PopupWithImage extends Popup {
+  constructor(
+    { name, link },
+    { popupSelector, imageSelector, imageTitleSelector }
+  ) {
+    super(popupSelector);
+    this._name = name;
+    this._link = link;
+    this._imageSelector = imageSelector;
+    this._imageTitleSelector = imageTitleSelector;
+    this._imageElement = this._popupElement.querySelector(this._imageSelector);
+    this._titleElement = this._popupElement.querySelector(
+      this._imageTitleSelector
+    );
+  }
+
+  _populateInfo() {
+    this._titleElement.textContent = this._name;
+    this._imageElement.src = this._link;
+    this._imageElement.alt = this._name;
+  }
+
+  open() {
+    this._populateInfo();
+    super.open();
+  }
+}
