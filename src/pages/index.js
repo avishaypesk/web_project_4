c61f488c697fd687746d5d720f21f16bf92a9dee
Avishay <avishaypesk@gmail.com>
2022-03-14 18:32:49 +0200

    moved index.js to pages dir

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import "../pages/index.css";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
-import Section from "./Section.js";
-import { Card } from "./Card.js";
-import { FormValidator } from "./FormValidator.js";
-import PopupWithImage from "./PopupWithImage.js";
+import PopupWithForm from "../scripts/PopupWithForm.js";
+import UserInfo from "../scripts/UserInfo.js";
+import Section from "../scripts/Section.js";
+import { Card } from "../scripts/Card.js";
+import { FormValidator } from "../scripts/FormValidator.js";
+import PopupWithImage from "../scripts/PopupWithImage.js";
 import {
   config,
   initialCards,
